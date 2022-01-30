5ee81edd08084080bab763eb70697cb162e77d9a
Dang Van Ty <dangvantydh1@gmail.com>
2022-01-30 16:17:00 +0700

    add code

---
+++ src/Components/Navbar.js
@@ -0,0 +1,34 @@
+import React,{useState} from 'react';
+import {Link}from'react-router-dom'
+function Navbar() {
+    const [click, setClick]=useState(false);
+    const handleClick=()=>setClick(!click);
+    const closeMobileMenu=()=>setClick(false);
+  return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to='/' className='navbar-logo'>
+                    TRVL <i className='fab fa-typo3'></i>
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times':'fas fa-bars'}></i>  
+                </div>
+                <ul className={click? 'nav-menu active':'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link> 
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>Services</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>Products</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </>
+  );
+}
+
+export default Navbar;
