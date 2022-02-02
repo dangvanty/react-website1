9688d8b8fa5b55329f40645aca836f24f7900502
Dang Van Ty <dangvantydh1@gmail.com>
2022-02-03 01:38:51 +0700

    herosection and card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
+import Home from './Components/Pages/Home';
+
 function App() {
   return (
     <>
       <Router>
         <Navbar/>
         <Routes>
-          <Route path='/' exact/>
+          <Route path='/' exact element={<Home/>}/>
+
         </Routes>
       </Router>
     </>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,21 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import {Link}from'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css'
 function Navbar() {
     const [click, setClick]=useState(false);
+    const [button,setButton]=useState(true);
     const handleClick=()=>setClick(!click);
     const closeMobileMenu=()=>setClick(false);
+    const showButton=()=>{
+        if(window.innerWidth <=960){
+            setButton(false)
+        }else{
+            setButton(true)
+        }
+    }
+    useEffect(()=>{ showButton()},[]);
+    window.addEventListener('resize',showButton);
   return (
     <>
         <nav className='navbar'>
@@ -22,9 +34,13 @@ function Navbar() {
                         <Link to='/services' className='nav-links' onClick={closeMobileMenu}>Services</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>Products</Link>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>Products</Link>
+                    </li>
+                    <li>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>Sign Up</Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline' >Sign Up</Button>}
             </div>
         </nav>
     </>
