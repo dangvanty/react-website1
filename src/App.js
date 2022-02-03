fada102818a0466709636a698d3145ba95d5e615
Dang Van Ty <dangvantydh1@gmail.com>
2022-02-03 11:23:18 +0700

    footer section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
 import Home from './Components/Pages/Home';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' exact element={<Home/>}/>
 
         </Routes>
+        <Footer/>
       </Router>
     </>
     
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../../App.css'
 import Cards from '../Cards'
+import Footer from '../Footer';
 import HeroSection from '../HeroSection';
 function Home() {
+  
   return(
       <>
+      <title>TRAVEL| Home</title>
        <HeroSection/>
        <Cards/>
       </>
